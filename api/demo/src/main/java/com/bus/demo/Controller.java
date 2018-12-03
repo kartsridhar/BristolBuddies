710690c6eb97d731b017c5aa46f859ccbfffbf08
AselKitulagoda <asel.kitulagoda@gmail.com>
2018-12-03 12:45:26 +0000

    Finished php version of login page

---
+++ api/demo/src/main/java/com/bus/demo/Controller.java
@@ -0,0 +1,13 @@
+package com.bus.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+
+    @GetMapping("getBusStopID")
+    public Integer getBusStopID(){
+
+    }
+}
