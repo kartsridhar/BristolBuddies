7a4b25d633f956caf07ab0fda7c9fa0fcb3d85bd
AselKitulagoda <asel.kitulagoda@gmail.com>
2019-02-04 13:19:50 +0000

    added change

--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/BristolBuddiesApplication.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/BristolBuddiesApplication.java
@@ -14,22 +14,22 @@ public class BristolBuddiesApplication {
 
 
 
-	public static final Optional<String> host;
-	public static final Optional<String> port;
+//	public static final Optional<String> host;
+//	public static final Optional<String> port;
 	public static final Properties myProps = new Properties();
 
 
-	static{
-		host = Optional.ofNullable(System.getenv("HOSTNAME"));
-		port = Optional.ofNullable(System.getenv("PORT"));
-	}
+//	static{
+//		host = Optional.ofNullable(System.getenv("HOSTNAME"));
+//		port = Optional.ofNullable(System.getenv("PORT"));
+//	}
 
 
 	public static void main(String[] args) {
-		myProps.setProperty("server.address",host.orElse("localhost"));
-		myProps.setProperty("server.port",port.orElse("8080"));
+//		myProps.setProperty("server.address",host.orElse("localhost"));
+//		myProps.setProperty("server.port",port.orElse("8080"));
 		SpringApplication app = new SpringApplication(BristolBuddiesApplication.class);
-		app.setDefaultProperties(myProps);
+//		app.setDefaultProperties(myProps);
 		app.run(args);
 
 	}
