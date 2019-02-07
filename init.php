95ee285adc8bc4c8f7ae52dae7038932a7b19b80
kartsridhar <contactkarthik34@gmail.com>
2019-02-07 20:45:07 +0000

    connection for android

---
+++ init.php
@@ -0,0 +1,7 @@
+<?php
+$db_name = "mydatabase";
+$mysql_username = "root";
+$mysql_pass = "Br!st0lBudd!e5";
+$server_name = "localhost";
+$con = mysqli_connect($server_name, $mysql_username, $mysql_pass, $db_name);
+?>
