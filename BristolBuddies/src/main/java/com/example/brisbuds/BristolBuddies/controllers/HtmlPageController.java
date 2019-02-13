1a6bf3566fcfbfcbc0cd1d722ea1b10a2d2c82ef
AselKitulagoda <asel.kitulagoda@gmail.com>
2019-02-13 11:21:31 +0000

    changed pom

--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/controllers/HtmlPageController.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/controllers/HtmlPageController.java
@@ -1,4 +1,16 @@
-package com.example.brisbuds.BristolBuddies.controllers;
-
-public class HtmlPageController {
-}
+//package com.example.brisbuds.BristolBuddies.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@Controller
+//public class HtmlPageController {
+//
+//    @RequestMapping("/")
+//    public ModelAndView index () {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("index");
+//        return modelAndView;
+//    }
+//    }
