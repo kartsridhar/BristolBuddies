fa2c4ee33dee1cd8cae9a56cc06d417cdabd4db5
adherse <alex.dherse@gmail.com>
2019-03-25 14:19:55 +0000

    Update buddies via username

--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/BuddyDAO.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/BuddyDAO.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface BuddyDAO {
      List<Buddy> getAllBuddies();
      boolean add(Buddy buddy);
+     boolean update(String id, Buddy buddy);
 }
--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/controllers/BuddyController.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/controllers/BuddyController.java
@@ -4,6 +4,8 @@ import com.example.brisbuds.BristolBuddies.Buddy;
 import com.example.brisbuds.BristolBuddies.ao.BuddyDAO;
 import com.example.brisbuds.BristolBuddies.ao.BuddyDbDAO;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +24,15 @@ public class BuddyController {
         return edao.getAllBuddies().toArray(new Buddy[0]);
     }
 
+    // Update a buddy
+    @RequestMapping(method = RequestMethod.PUT, value = "{username}")
+    public ResponseEntity update(@PathVariable String username, @RequestBody Buddy student) {
+
+        if (edao.update(username, student)) {
+            return new ResponseEntity<>(null, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
