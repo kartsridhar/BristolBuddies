328e8c4c2780dd1325871ff9c53a5b8d6276dfa1
kartsridhar <contactkarthik34@gmail.com>
2019-03-25 13:03:13 +0000

    added controller tests for Students and Events

---
+++ BristolBuddies/src/test/java/com/example/brisbuds/BristolBuddies/APITests/StudentAPITest.java
@@ -0,0 +1,51 @@
+package com.example.brisbuds.BristolBuddies.APITests;
+
+import com.example.brisbuds.BristolBuddies.BristolBuddiesApplicationTests;
+import com.example.brisbuds.BristolBuddies.controllers.StudentController;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+public class StudentAPITest extends BristolBuddiesApplicationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private StudentController studentController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testStudentControllerNull() {
+        assertThat(studentController).isNotNull();
+    }
+
+    // Application starts then the test call will be made
+    @Test
+    public void testGetRespectiveStudent() throws Exception {
+        mockMvc.perform(get("/students")).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].firstName").value("Tony"))
+                .andExpect(jsonPath("$[0].lastName").value("Stark"))
+                .andExpect(jsonPath("$[0].gender").value("Male"))
+                .andExpect(jsonPath("$[0].userName").value("ab12312"))
+                .andExpect(jsonPath("$[0].password").value("starkintdustreks"))
+                .andExpect(jsonPath("$[0].department").value("Arts"))
+                .andExpect(jsonPath("$[0].yearofStudy").value("4"));
+    }
+}
