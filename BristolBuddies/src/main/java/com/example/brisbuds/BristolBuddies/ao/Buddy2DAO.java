e7941e231601c1f184dcaecfae6b72c5000eabcf
Asel Kitulagoda <asel.kitulagoda@gmail.com>
2019-04-11 18:26:11 +0100

    Testing with buddy2 again

--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/Buddy2DAO.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/Buddy2DAO.java
@@ -4,6 +4,6 @@ import com.example.brisbuds.BristolBuddies.Buddy;
 
 public interface Buddy2DAO {
      boolean update(Buddy buddy);
-//     boolean add(Buddy buddy);
+     boolean add(Buddy buddy);
 
 }
--- BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/Buddy2DbDAO.java
+++ BristolBuddies/src/main/java/com/example/brisbuds/BristolBuddies/ao/Buddy2DbDAO.java
@@ -43,40 +43,40 @@ public class Buddy2DbDAO implements Buddy2DAO {
         }
     }
 
-//    @Override
-//    public boolean add(Buddy buddy) {
-//        String insertTableSQL = "INSERT INTO BUDDIES"
-//                + "(FIRSTNAME, LASTNAME, USERNAME, COURSE, NATIONALITY, INTERESTS, PERSONALITY, PREFERENCES, PASSWORD, NUMBEROFMATCHES, STUDENT1ID, STUDENT2ID, STUDENT3ID) "
-//                + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);";
-//
-//        try (PreparedStatement preparedStatement = this.conn
-//                .prepareStatement(insertTableSQL)) {
-//
-//            preparedStatement.setString(1, buddy.getFirstName());
-//            preparedStatement.setString(2, buddy.getLastName());
-//            preparedStatement.setString(3, buddy.getUsername());
-//            preparedStatement.setString(4, buddy.getCourse());
-//            preparedStatement.setString(5, buddy.getNationality());
-//            preparedStatement.setString(6, buddy.getInterests());
-//            preparedStatement.setString(7, buddy.getPersonality());
-//            preparedStatement.setString(8, buddy.getPreferences());
-//            preparedStatement.setString(9, buddy.getPassword());
-//            preparedStatement.setString(10, Integer.toString(buddy.getNumberOfMatches()));
-//            preparedStatement.setString(11, buddy.getStudent1ID());
-//            preparedStatement.setString(12, buddy.getStudent2ID());
-//            preparedStatement.setString(13, buddy.getStudent3ID());
-//
-//
-//            preparedStatement.executeUpdate();
-//            return true;
-//        } catch (SQLException e) {
-//            System.out.println("SQL Add Error: " + e.getMessage());
-//            return false;
-//        } catch (Exception e) {
-//            System.out.println("Add Error: " + e.getMessage());
-//            return false;
-//        }
-//    }
+    @Override
+    public boolean add(Buddy buddy) {
+        String insertTableSQL = "INSERT INTO BUDDIES"
+                + "(FIRSTNAME, LASTNAME, USERNAME, COURSE, NATIONALITY, INTERESTS, PERSONALITY, PREFERENCES, PASSWORD, NUMBEROFMATCHES, STUDENT1ID, STUDENT2ID, STUDENT3ID) "
+                + "VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?);";
+
+        try (PreparedStatement preparedStatement = this.conn
+                .prepareStatement(insertTableSQL)) {
+
+            preparedStatement.setString(1, buddy.getFirstName());
+            preparedStatement.setString(2, buddy.getLastName());
+            preparedStatement.setString(3, buddy.getUsername());
+            preparedStatement.setString(4, buddy.getCourse());
+            preparedStatement.setString(5, buddy.getNationality());
+            preparedStatement.setString(6, buddy.getInterests());
+            preparedStatement.setString(7, buddy.getPersonality());
+            preparedStatement.setString(8, buddy.getPreferences());
+            preparedStatement.setString(9, buddy.getPassword());
+            preparedStatement.setString(10, Integer.toString(buddy.getNumberOfMatches()));
+            preparedStatement.setString(11, buddy.getStudent1ID());
+            preparedStatement.setString(12, buddy.getStudent2ID());
+            preparedStatement.setString(13, buddy.getStudent3ID());
+
+
+            preparedStatement.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("SQL Add Error: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Add Error: " + e.getMessage());
+            return false;
+        }
+    }
 
 }
 
